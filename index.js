63bf66bd15295b6f33aac9e373f180f22d069d85
Md-Atikul-Islam-420 <mdatikulislam2314@gmail.com>
2025-02-20 03:47:32 +0600

    variable create

---
+++ index.js
@@ -0,0 +1,6 @@
+//Variable create
+
+const todoInput = document.getElementById('todo-input');
+const addBtn = document.getElementById('add-btn');
+const todoList = document.getElementById('todo-list');
+
